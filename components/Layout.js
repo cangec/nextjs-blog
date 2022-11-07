a67c0a32943f99cb0651299918da8c804c673136
Gorazd Stane <gorazdstane@Gorazds-MacBook-Pro.local>
2022-11-07 14:18:57 +0100

    first commit

--- components/Layout.js
+++ components/Layout.js
@@ -4,10 +4,11 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Your Name';
+const name = 'Gorazd Stane';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
+
     return (
         <div className={styles.container}>
             <Head>
@@ -30,7 +31,7 @@ export default function Layout({ children, home }) {
                     <>
                         <Image
                             priority
-                            src="/images/profile.jpg"
+                            src="/images/profile.jpeg"
                             className={utilStyles.borderCircle}
                             height={144}
                             width={144}
@@ -41,20 +42,20 @@ export default function Layout({ children, home }) {
                 ) : (
                     <>
                         <Link href="/">
-                            <a>
-                                <Image
-                                    priority
-                                    src="/images/profile.jpg"
-                                    className={utilStyles.borderCircle}
-                                    height={108}
-                                    width={108}
-                                    alt=""
-                                />
-                            </a>
+
+                            <Image
+                                priority
+                                src="/images/profile.jpeg"
+                                className={utilStyles.borderCircle}
+                                height={108}
+                                width={108}
+                                alt=""
+                            />
+
                         </Link>
                         <h2 className={utilStyles.headingLg}>
-                            <Link href="/">
-                                <a className={utilStyles.colorInherit}>{name}</a>
+                            <Link href="/" className={utilStyles.colorInherit}>
+                                {name}
                             </Link>
                         </h2>
                     </>
@@ -64,7 +65,7 @@ export default function Layout({ children, home }) {
             {!home && (
                 <div className={styles.backToHome}>
                     <Link href="/">
-                        <a>← Back to home</a>
+                        ← Back to home
                     </Link>
                 </div>
             )}
